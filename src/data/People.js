725baf277478c6c294084fa3bacb35d9f9f38820
Johan Garzon <johanprograma@hotmail.com>
2020-12-27 15:11:14 +0100

    Can list the first 10 people from SWAPI on People Page

--- src/data/People.js
+++ src/data/People.js
@@ -7,11 +7,11 @@ const peopleUrl = `${apiBaseUrl}/people`
   next and previous url pagination from SWAPI
 */
 function fetchPeople(url = peopleUrl) {
-  fetch(url)
+  return fetch(url)
   .then( res => res.json())
   .then( data => {
     console.log('data', data)
-    //return data
+    return data
   })
 }
 
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,13 +1,26 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { fetchPeople } from '../data/People'
 
 export default function People() {
-  
+  const [people, setPeople] = useState(null)
+
+  async function getPeople() {
+    const infoFetched = await fetchPeople()
+    setPeople(infoFetched.results)
+  }
+
   useEffect( () => {
-    fetchPeople()
+    getPeople()
   }, [])
 
   return (
-    <h2>People list page</h2>
+    <>
+      <h2>People list page</h2> 
+      {
+        people && people.map( (person, index) => {
+          return <p key={index}> {person.name} </p>
+        })
+      }
+    </>
   )
 }
