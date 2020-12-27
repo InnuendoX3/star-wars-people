892be5acf702b34aacf0425bac608ded0589a183
Johan Garzon <johanprograma@hotmail.com>
2020-12-27 13:15:24 +0100

    Install react-router-dom / Delete some create-react-app stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Star Wars People</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This should appear when deploying on Heroku!
-        </p>
-      </header>
+    <div>
+      <h1>Star Wars People</h1>
+      <Switch>
+        <Route path="/search" >
+          <SearchResults />
+        </Route>
+        <Route path="/">
+          <People />
+        </Route>
+
+      </Switch>
     </div>
   );
 }
