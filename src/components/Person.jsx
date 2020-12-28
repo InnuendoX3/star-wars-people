3150babd45591f19b0d9d15cfdf36ca9f3f0e428
Johan Garzon <johanprograma@hotmail.com>
2020-12-28 17:49:51 +0100

    Basic show/hide of extra information when clicking name

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Person(props) {
   const { name, gender, height, mass } = props.personData
+  const [showMore, setShowMore] = useState(false)
+
+  function clickOnName() {
+    setShowMore(!showMore)
+  }
+
   return (
     <div>
-      <h4>{name}</h4>
-      <div>
-        <p>{gender}</p>
-        <p>{height}</p>
+      <h4 onClick={clickOnName}>{name}</h4>
+      <div className={showMore ? 'show' : 'hide'}>
+        <p >{gender}</p>
+        <p className="blue">{height}</p>
         <p>{mass}</p>
       </div>
     </div>
