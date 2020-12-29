2a7015202736cdf064da9366816c21fd7f4816b5
Johan Garzon <johanprograma@hotmail.com>
2020-12-29 10:16:05 +0100

    SearchContext works when submiting some text on SearchForm

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {SearchContext} from '../context/SearchContext'
 
 export default function SearchResults() {
+  const {searchText} = useContext(SearchContext)
   return (
-    <h2>Search Results page</h2>
+    <>
+      <h2>Search Results page</h2>
+      <h3>{searchText}</h3>
+    </>
   )
 }
