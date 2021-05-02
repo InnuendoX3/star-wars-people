a6492efe310b4df205954626f06418552b739bcb
Johan Garzon <johanprograma@hotmail.com>
2021-05-02 11:50:28 +0200

    Debuging fetch

--- src/data/People.js
+++ src/data/People.js
@@ -7,9 +7,13 @@ const searchUrl = `${apiBaseUrl}/people/?search=`
 // Parameter: peopleUrl as default but it will use
 // next and previous url pagination from SWAPI
 async function fetchPeople(url = peopleUrl) {
+  console.log('fetchPeople url', url)
   return fetch(url)
   .then( res => res.json())
-  .then( data => data )
+  .then( data => {
+    console.log('fetchPeople data', data)
+    return data
+  } )
 }
 
 async function fetchSearchPeople(toSearch) {
