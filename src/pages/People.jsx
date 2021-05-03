f2c5c312d177fd6f6a5577769eb66a9b8b20658d
Johan Garzon <johanprograma@hotmail.com>
2021-05-03 08:05:55 +0200

    Move getSwapiData into the useEffect

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -12,24 +12,20 @@ export default function People() {
   const [previousUrl, setPreviousUrl] = useState(null)
   const [currentPage, setCurrentPage] = useState(null)
 
-  // Initial fetch
-  async function getSwapiData() {
-    const infoFetched = await fetchPeople()
-    console.log('infoFetched', infoFetched)
-    const pages = Math.ceil(infoFetched.count/10)
-    setPeople(infoFetched.results)
-    setPreviousUrl(infoFetched.previous)
-    setNextUrl(infoFetched.next)
-    setTotalPages( pages > 1 ? pages : null) //Show if more than 1 page
-    setCurrentPage(1)
+  function changeToSecureUrl (httpUrl) {
+    if (!httpUrl) return 
+    console.log('changeToSecureUrl url', httpUrl)
+    const httpsUrl = httpUrl.replace('http://', 'https://')
+    console.log('httpsUrl url', httpsUrl)
+    return httpsUrl
   }
 
   // Fetch from previous/next pagination
   async function getMorePeople(url) {
     const infoFetched = await fetchPeople(url)
     setPeople(infoFetched.results)
-    setPreviousUrl(infoFetched.previous)
-    setNextUrl(infoFetched.next)
+    setPreviousUrl(changeToSecureUrl(infoFetched.previous))
+    setNextUrl(changeToSecureUrl(infoFetched.next))
   }
 
   function handlePrevious() {
@@ -42,11 +38,21 @@ export default function People() {
   }
 
   useEffect( () => {
-    try {
-      getSwapiData()
-    } catch (error) {
-      console.log('useEffect TryCatch',error)
-    }
+  // Initial fetch
+  async function getSwapiData() {
+    const infoFetched = await fetchPeople()
+    console.log('infoFetched', infoFetched)
+    const pages = Math.ceil(infoFetched.count/10)
+    setPeople(infoFetched.results)
+    console.log('infoFetched.previous', infoFetched.previous)
+    console.log('infoFetched.next', infoFetched.next)
+    setPreviousUrl(changeToSecureUrl(infoFetched.previous))
+    setNextUrl(changeToSecureUrl(infoFetched.next))
+    setTotalPages( pages > 1 ? pages : null) //Show if more than 1 page
+    setCurrentPage(1)
+  }
+
+    getSwapiData()
   }, [])
 
   const paginationProps = {
